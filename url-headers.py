dfa691d226b894ab80c4c88fbf6ed5f333ee7dae
stark0de <53110298+stark0de@users.noreply.github.com>
2021-04-07 11:32:29 +0200

    Update url-headers.py

--- url-headers.py
+++ url-headers.py
@@ -3,6 +3,8 @@ import sys
 
 onetwosevendict={}
 localhostdict={}
+oneninetwodict={}
+tenzerozerodict={}
 
 complete_header_list = [
     "Proxy-Host","Request-Uri","X-Forwarded","X-Forwarded-By","X-Forwarded-For",
@@ -10,13 +12,16 @@ complete_header_list = [
     "X-Forward-For","Base-Url","Http-Url","Proxy-Url","Redirect","Real-Ip","Referer",
     "Referrer","Uri","Url","X-Host","X-Http-Destinationurl","X-Http-Host-Override",
     "X-Original-Remote-Addr","X-Original-Url","X-Proxy-Url","X-Rewrite-Url","X-Real-Ip","X-Remote-Addr", "X-Proxy-URL", "X-Original-Host", "X-Originally-Forwarded-For", "X-Forwarded-For-Original",
-    "X-Originating-Ip","X-Ip", "X-Client-Ip"
+    "X-Originating-Ip","X-Ip", "X-Client-Ip", "X-Real-Ip"
     ]
 for i in complete_header_list:
     onetwosevendict.update({i: "127.0.0.1"})
 for i in complete_header_list:
     localhostdict.update({i: "localhost"})
-           
+for i in complete_header_list:
+    oneninetwodict.update({i: "192.168.1.1"})
+for i in complete_header_list:
+    tenzerozerodict.update({i: "10.0.0.1"})
 counter=0
 
 r_first= requests.get(sys.argv[1]) #copy as Python request in Burp if you are testing an authenticated thing/POST request/API
@@ -44,4 +49,34 @@ for x, y in localhostdict.items():
        counter+=1
 if counter == 0:
    print("No relevant results for localhost tests")
+
+counter=0
+
+r_first= requests.get(sys.argv[1]) #copy as Python request in Burp if you are testing an authenticated thing/POST request/API
+for x, y in oneninetwodict.items():
+    z = {x:y}
+    r = requests.get(sys.argv[1], headers=z)
+    resta = len(r.text) - len(r_first.text)
+    if r.status_code != r_first.status_code or resta > 50:
+       print("Difference found in :" + sys.argv[1] + " with headers:")
+       print(r.request.headers)
+       counter+=1
+if counter == 0:
+   print("No relevant results for 192.168.1.1 tests")
+
+counter=0
+
+r_first= requests.get(sys.argv[1]) #copy as Python request in Burp if you are testing an authenticated thing/POST request/API
+for x, y in tenzerozerodict.items():
+    z = {x:y}
+    r = requests.get(sys.argv[1], headers=z)
+    resta = len(r.text) - len(r_first.text)
+    if r.status_code != r_first.status_code or resta > 50:
+       print("Difference found in :" + sys.argv[1] + " with headers:")
+       print(r.request.headers)
+       counter+=1
+if counter == 0:
+   print("No relevant results for 10.0.0.1 tests")
+
+
 
